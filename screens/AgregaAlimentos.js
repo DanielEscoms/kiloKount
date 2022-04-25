971e0fbc0ea83834e1a0ae7e14cf90920dae2e50
Daniel Escoms <daesdo@floridauniversitaria.es>
2022-04-25 20:31:04 +0200

    Consulta a la API calorieninjas correcta

--- screens/AgregaAlimentos.js
+++ screens/AgregaAlimentos.js
@@ -1,3 +1,5 @@
+/* eslint-disable quotes */
+/* eslint-disable no-undef */
 /* eslint-disable jsx-quotes */
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable semi */
@@ -10,18 +12,83 @@ import axios from 'axios';
 
 const AgregaAlimentos = () => {
   //llamadas axios a API
-  //const [datos, setDatos] = useState();
-  const [imagenPerro, setImagenPerro] = useState();
+  const [alimentoBuscado, setAlimentoBuscado] = useState("potato");
+  const [datos, setDatos] = useState([]);
+  const [calories, setCalories] = useState("");
+  const [carbohiratosTotalG, setCarbohidratosTotalG] = useState("");
+  const [colesterolMg, setColesterolMg] = useState("");
+  const [grasasSaturadasG, setGrasasSaturadasG] = useState("");
+  const [grasasTotalG, setGrasasTotalG] = useState("");
+  const [fibraG, setFibraG] = useState("");
+  const [nombre, setNombre] = useState("");
+  const [potasioMg, setPotasioMg] = useState("");
+  const [proteinaG, setProteinaG] = useState("");
+  const [cantidadG, setCantidadG] = useState("");
+  const [sodioMg, setSodioMg] = useState("");
+  const [azucarG, setAzucarG] = useState("");
+  //const [imagenPerro, setImagenPerro] = useState();
   
-  useEffect(() =>{
+  /*useEffect(() =>{
     getDatos();
-  },[])
+  },[])*/
   
-  const getDatos = async() => {
-    const resultado = await axios.get('https://dog.ceo/api/breeds/image/random');
-    setImagenPerro(resultado.data.message);
-    console.log('imagen del Perro: ', imagenPerro);
+  
+  /*const getDatos = () => {
+    $.ajax({
+      method: 'GET',
+      url: `https://api.calorieninjas.com/v1/nutrition?query=${alimentoBuscado}`,
+      headers: { 'X-Api-Key': 'nLw46OBFPXRZtypyMJcJ6Q==h6CNwrcwmBhXEgOX'},
+      contentType: 'application/json',
+      success: function(result) {
+          console.log(result);
+      },
+      error: function ajaxError(jqXHR) {
+          console.error('Error: ', jqXHR.responseText);
+      },
+    });
+  }*/
+  
+  const options = { 
+    method: 'GET', 
+    url: `https://api.calorieninjas.com/v1/nutrition?query=${alimentoBuscado}`,
+    headers: { 'X-Api-Key': 'nLw46OBFPXRZtypyMJcJ6Q==h6CNwrcwmBhXEgOX'},
+  };
+    
+  const getDatos = () => {
+    axios.request(options).then(function (response) {
+    console.log(response.data);
+    setDatos(response.data.items);
+    console.log(response.data.items);
+    setCalories(response.data.items[0].calories);
+    console.log(response.data.items[0].calories);
+    setCarbohidratosTotalG(response.data.items[0].carbohydrates_total_g);
+    console.log(response.data.items[0].carbohydrates_total_g);
+    setColesterolMg(response.data.items[0].cholesterol_mg);
+    console.log(response.data.items[0].cholesterol_mg);
+    setGrasasSaturadasG(response.data.items[0].fat_saturated_g);
+    console.log(response.data.items[0].fat_saturated_g);
+    setGrasasTotalG(response.data.items[0].fat_total_g);
+    console.log(response.data.items[0].fat_total_g);
+    setFibraG(response.data.items[0].fiber_g);
+    console.log(response.data.items[0].fiber_g);
+    setNombre(response.data.items[0].name);
+    console.log(response.data.items[0].name);
+    setPotasioMg(response.data.items[0].potassium_mg);
+    console.log(response.data.items[0].potassium_mg);
+    setProteinaG(response.data.items[0].protein_g);
+    console.log(response.data.items[0].protein_g);
+    setCantidadG(response.data.items[0].serving_size_g);
+    console.log(response.data.items[0].serving_size_g);
+    setSodioMg(response.data.items[0].sodium_mg);
+    console.log(response.data.items[0].sodium_mg);
+    setAzucarG(response.data.items[0].sugar_g);
+    console.log(response.data.items[0].sugar_g);
+    }).catch(function (error) { 
+    console.error(error);
+    });
   }
+
+
   return (
     <View style={styles.containerPage}>
       <View style={styles.container1}>
@@ -29,20 +96,33 @@ const AgregaAlimentos = () => {
           <Text style={styles.text}>Alimento: </Text>
           <Input
             placeholder='Introduce el alimento'
-            onChangeText={this.guardaPeso}
+            onChangeText={setAlimentoBuscado}
           />
+          <Text>{alimentoBuscado}</Text>
+          <Text>{calories}</Text>
+          <Text>{carbohiratosTotalG}</Text>
+          <Text>{colesterolMg}</Text>
+          <Text>{grasasSaturadasG}</Text>
+          <Text>{grasasTotalG}</Text>
+          <Text>{fibraG}</Text>
+          <Text>{nombre}</Text>
+          <Text>{potasioMg}</Text>
+          <Text>{proteinaG}</Text>
+          <Text>{cantidadG}</Text>
+          <Text>{sodioMg}</Text>
+          <Text>{azucarG}</Text>
         </View>
         <View>
           <Button
             title='Consultar Alimento'
             raised={true}
-            onPress={this.calculaIMC}
+            onPress={getDatos}
           />
         </View>
         <View>
           <Text style={styles.text}>Cantidad: </Text>
           <Input
-            placeholder='Introduce el peso del alimento'
+            placeholder='Introduce el peso en gramos del alimento'
             keyboardType='decimal-pad'
             onChangeText={this.guardaPeso}
           />
@@ -56,10 +136,7 @@ const AgregaAlimentos = () => {
         />
       </View>
       <View style={styles.layout}>
-        <Image
-          style = {styles.avatar}
-          source={{uri : imagenPerro}}
-        />
+    
       </View>
     </View>
   )
@@ -80,12 +157,6 @@ const styles = StyleSheet.create({
   },
   containerButton:{
     padding: 10,
-  },
-  avatar: {
-    height: 128,
-    width: 128,
-    borderRadius: 15,
-    alignSelf: 'center',
   },
   layout: {
     flex: 1,
