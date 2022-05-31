4021686d0caae5446c3342096a937675e65c3aa5
DESKTOP-5L88R7O\Daniel Escoms <daesdo@floridauniversitaria.es>
2022-05-31 12:10:22 +0200

    Retoque en agrega alimento al testear por persona no cualificada.

--- screens/AgregaAlimentos/AgregaAlimentos.js
+++ screens/AgregaAlimentos/AgregaAlimentos.js
@@ -134,6 +134,7 @@ const AgregaAlimentos = (props) => {
       setArrayAlimento([{ "calories": 0, "carbohydrates_total_g": 0, "cholesterol_mg": 0, "fat_saturated_g": 0, "fat_total_g": 0, "fiber_g": 0, "name": "", "potassium_mg": 0, "protein_g": 0, "serving_size_g": 0, "sodium_mg": 0, "sugar_g": 0 }]);
       //limpiar valor a buscar
       setAlimentoBuscado("");
+      setPesoIntroducido("");
     } else {
       alert("Añade un peso en gramos válido.");
       return;
